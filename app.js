851898d0dc83c710b1e4c44da080699c5201eca6
Cassius <casskomp@gmx.de>
2024-03-28 11:11:25 +0100

    bo more public

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Serve static files from 'public' directory
+// app.use(express.static('public')); // Serve static files from 'public' directory
 
 const { setReminder } = require('./bot');
 
