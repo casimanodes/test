8a984b59f161a45da83dc05fba0d043b1ec4b691
Cassius <casskomp@gmx.de>
2024-07-02 16:11:15 +0200

    design

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,54 +1,34 @@
 
 // CHATBOT
-const chatbot = document.querySelector('.chatbot');
-const chatInput = document.getElementById('chatInput');
-const chatClose = document.querySelector('.close');
-const chatBotText = document.querySelector(".chatbotText");
-const inputField = document.querySelector('.input_send');
-const pos_spinner = document.querySelector('.pos_spinner');
-const chatbot_svg = document.querySelector('.chatbot_svg');
-const chatbotTitle = document.querySelector('.chatbotTitle');
+const open_chatbot = document.querySelector('.chatbot');
+const svg_open_chatbot = document.querySelector('.open_chatbot');
+const close_chatbot = document.querySelector('.close_chatbot');
+const chatbotContainer = document.querySelector('.chatbotContainer');
 
-document.querySelector('.chatbot').addEventListener('click', function() {
-  chatbot.classList.add('expanded');
-  chatInput.style.display = "flex";
-  chatClose.style.pointerEvents = "all";
-  chatClose.style.opacity = "1";
-  chatClose.style.zIndex = "999"; 
-  chatBotText.style.visibility = "visible";
-  inputField.style.display = "flex";
-  chatbot_svg.style.display = "none";
-  chatbotTitle.style.display = "flex";
-  // pos_spinner.style.display = "block";
-  // chatBotText.scrollTop = chatBotText.scrollHeight;
-});
+let chatbot_opened = false;
 
-function closeBot() {
-  chatbot.classList.remove('expanded');
-  chatInput.style.display = "none";
-  chatClose.style.zIndex = "";
-  chatClose.style.pointerEvents = "none";
-  chatBotText.style.visibility = "hidden";
-  inputField.style.display = "";
-  // pos_spinner.style.display = "";
-  chatbotTitle.style.display = "none";
+document.querySelector('.chatbot_icons').addEventListener('click', function() {
+  if(!chatbot_opened){
+    open_chatbot.style.position = "fixed";
+    open_chatbot.classList.add('expanded');
+    chatbotContainer.style.display = "flex";
+    svg_open_chatbot.style.display = "none";
 
-  setTimeout(function() {
-    chatClose.style.opacity = "0";
-  chatbot_svg.style.display = "";
-
-  }, 300);
-}
-
-
-document.addEventListener('click', function(e) {
-  // Überprüfen Sie, ob das geklickte Element nicht der Chatbot ist
-  if (!e.target.closest('.chatbot')) {
-    closeBot();
+    // add close button to chatbot
+    close_chatbot.style.display = "block";
+    close_chatbot.style.pointerEvents = "all";
+    close_chatbot.style.opacity = "1";
+    close_chatbot.style.zIndex = "999"; 
+    chatbot_opened = true;
+  } else {
+    // close entfernen 
+    close_chatbot.style.display = "none";
+    close_chatbot.style.pointerEvents = "none";
+    close_chatbot.style.opacity = "0";
+    close_chatbot.style.zIndex = ""; 
+    open_chatbot.classList.remove('expanded');
+    chatbotContainer.style.display = "none";
+    svg_open_chatbot.style.display = "block";
+    chatbot_opened = false;
   }
 });
-document.querySelector('.close').addEventListener('click', function() {
-  closeBot();
-});
-
-
